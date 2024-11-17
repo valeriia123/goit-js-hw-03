dbb523f6e40a64d5ad141ef4c7660c64b13b5338
Valeriia Pylypenko <pylypenko.valeriia1@gmail.com>
2024-11-17 17:44:40 +0000

    first commit

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+const makeArray = function (firstArray, secondArray, maxLength) {
+    let totalArr = firstArray.concat(secondArray);
+    return totalArr.length <= maxLength ? totalArr : totalArr.slice(0, maxLength);
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
